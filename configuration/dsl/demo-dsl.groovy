01177d1f526dece41e15172869f4259bdc877a0e
Mradul Pandey <pandeymradul@gmail.com>
2018-12-08 16:03:53 +0530

    first commit

---
+++ configuration/dsl/demo-dsl.groovy
@@ -0,0 +1,7 @@
+@Library("jenkins-demo-shared-libraries") _
+
+demoPipline(
+        branch: "master",
+        gitUrl: "https://github.com/jinternals/spring-micrometer-demo.git",
+        email: "pandeymradul@gmail.com"
+)
