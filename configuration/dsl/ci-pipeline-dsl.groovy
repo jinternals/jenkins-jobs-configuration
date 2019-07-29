a9788195b5d192b3aec5ce0d10bd4051d0edde34
Mradul <pandeymradul@gmail.com>
2019-07-29 18:19:20 +0530

    Update ci-pipeline-dsl.groovy

--- configuration/dsl/ci-pipeline-dsl.groovy
+++ configuration/dsl/ci-pipeline-dsl.groovy
@@ -1,5 +1,8 @@
 @Library("jenkins-demo-shared-libraries") _
 
 ciPipeline(
-    email: "pandeymradul@gmail.com"
+    email: "pandeymradul@gmail.com",
+    dockerCredentialId: "dockerhub",
+    dockerRegistry: "https://index.docker.io/v1/",
+    dockerRepository: "{dockerRepository}"
 )
