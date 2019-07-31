c6f82e183a53013e0e72d598e8bb97c2a7253519
Mradul <pandeymradul@gmail.com>
2019-08-01 00:21:23 +0530

    Update git-pull-request-pipeline-dsl.groovy

--- configuration/dsl/git-pull-request-pipeline-dsl.groovy
+++ configuration/dsl/git-pull-request-pipeline-dsl.groovy
@@ -1,4 +1,7 @@
 
 @Library("jenkins-demo-shared-libraries") _
 
-gitPullRequestPipeline(name: 'some-name')
+gitPullRequestPipeline(name: 'some-name'
+                       gitBranch: "{gitBranch}",
+                       gitCredentialId: "github",
+                       gitRepository: "{gitRepository}")
